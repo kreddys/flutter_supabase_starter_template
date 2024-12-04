6f32a1c78760a48126c48a879be3b7b48ef1eb09
kreddy <kishore5214@outlook.com>
2024-12-03 22:11:32 -0600

    added more sentry code

--- flutter_application/lib/features/user/domain/use_case/change_email_address_use_case.dart
+++ flutter_application/lib/features/user/domain/use_case/change_email_address_use_case.dart
@@ -1,6 +1,9 @@
 import 'package:amaravati_chamber/core/use_cases/use_case.dart';
 import 'package:amaravati_chamber/features/user/domain/repository/user_repository.dart';
 import 'package:injectable/injectable.dart';
+import '../../../../core/monitoring/sentry_monitoring.dart';
+import 'package:sentry/sentry.dart';
+import 'package:amaravati_chamber/features/user/domain/exception/change_email_address_exception.dart'; 
 
 @injectable
 class ChangeEmailAddressUseCase extends UseCase<void, ChangeEmailAddressUseCaseParams> {
@@ -11,8 +14,24 @@ class ChangeEmailAddressUseCase extends UseCase<void, ChangeEmailAddressUseCaseP
   final UserRepository _userRepository;
 
   @override
-  Future<void> execute(ChangeEmailAddressUseCaseParams params) {
-    return _userRepository.changeEmailAddress(params.email);
+  Future<void> execute(ChangeEmailAddressUseCaseParams params) async {
+    final transaction = SentryMonitoring.startTransaction(
+      'change_email_address',
+      'email_operation',
+    );
+
+    try {
+      await _userRepository.changeEmailAddress(params.email);
+      transaction.finish(status: const SpanStatus.ok());
+    } catch (error, stackTrace) {
+      transaction.finish(status: const SpanStatus.internalError());
+      await SentryMonitoring.captureException(
+        error,
+        stackTrace,
+        tagValue: 'change_email_failure',
+      );
+      throw ChangeEmailAddressException(message: error.toString());
+    }
   }
 }
 
