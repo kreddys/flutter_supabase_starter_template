fe951d0978d0c9fe6d6fb7627651e6271dfc95aa
kreddy <kishore5214@outlook.com>
2024-12-04 23:18:28 -0600

    trying to add tests..

---
+++ flutter_application/lib/features/news/presentation/bloc/news_cubit_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:amaravati_chamber/features/news/domain/repositories/i_news_repository.dart';
+import 'package:amaravati_chamber/features/news/presentation/bloc/news_cubit.dart';
+import 'package:amaravati_chamber/core/voting/domain/repositories/i_voting_repository.dart';
+
+@GenerateMocks([INewsRepository, IVotingRepository])
+void main() {
+  // Tests will go here
+}
